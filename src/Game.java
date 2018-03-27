843fd1c89d6582098e55ac0dac946ca2016e2545
unknown <nobbe1@purdue.edu>
2018-03-27 10:30:34 -0400

    2nd commit, added game loop

--- src/Game.java
+++ src/Game.java
@@ -9,15 +9,61 @@ public class Game extends Canvas implements Runnable {
     public Game() {
         new Window(WIDTH, HEIGHT, "Let's Build a Game!", this);
     }
-    public static synchronized void start() {
 
+    private Thread thread;
+    private boolean running = false;
+
+    public synchronized void start() {
+        thread = new Thread(this);
+        thread.start();
+        running = true;
+    }
+
+    public synchronized void stop() {
+        try {
+            thread.join();
+            running = false;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void run() {
+        // Creating the "Game Loop"
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int frames = 0;
+        while(running){
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            while(delta >= 1){
+                tick();
+                delta--;
+            }
+            if(running)
+                render();
+            frames++;
+
+            if(System.currentTimeMillis() - timer > 1000){
+                timer += 1000;
+                System.out.println("FPS: " + frames);
+                frames = 0;
+            }
+        }
+        stop();
+    }
+    private void tick() {
 
     }
 
+    private void render() {
+
+    }
     public static void main(String[] args) {
         new Game();
     }
